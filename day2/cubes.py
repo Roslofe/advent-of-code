1877b679e272f376fa71a281693748e14fa438c6
Eli Roslöf <eli.roslof@gmail.com>
2023-12-02 19:37:35 +0200

    Add the second half of day 2

--- day2/cubes.py
+++ day2/cubes.py
@@ -1,5 +1,6 @@
 from cube import Cube
 import re
+from functools import reduce
 
 
 # parse the data file to get only the lines
@@ -13,24 +14,59 @@ def parse(filename):
     except OSError:
         print("Error in reading the file.")
 
-    return lines
+    return list(lines)
 
 
-def main():
-    lines = parse("day2/data.txt")
+# turn the line of data into a list of Cube objects
+def as_cubes(line):
+    return list(
+        map(
+            lambda n: Cube(n.split()[0], n.split()[1]),
+            re.split(", |; ", line),
+        )
+    )
+
+
+# calculate sum of all game IDs where none of the cube amounts go over the limit
+def id_sum(lines):
     sum = 0
     for line in lines:
         split_line = line.split(": ")
         id = int(split_line[0].split()[1])
-        as_cubes = list(
-            map(
-                lambda n: Cube(n.split()[0], n.split()[1]),
-                re.split(", |; ", split_line[1]),
-            )
-        )
-        if not any(cube.too_many() for cube in as_cubes):
+        cubes = as_cubes(split_line[1])
+        if not any(cube.too_many() for cube in cubes):
             sum += id
-    print(f"Sum of IDs from valid games: {sum}")
+    return sum
+
+
+# calculate the sum of the powers of the smallest possible cube amounts in each game
+def smallest_power(lines):
+    sum = 0
+    for line in lines:
+        cubes = as_cubes(line.split(": ")[1])
+        r = -1
+        g = -1
+        b = -1
+        cubes_sorted = sorted(cubes, key=lambda x: x.amount())
+        while cubes_sorted and (r == -1 or g == -1 or b == -1):
+            curr_cube = cubes_sorted.pop()
+            match curr_cube:
+                case red if red.color() == "r":
+                    r = max(r, red.amount())
+                case green if green.color() == "g":
+                    g = max(g, green.amount())
+                case blue if blue.color() == "b":
+                    b = max(b, blue.amount())
+        sum += reduce(lambda x, y: x * y, filter(lambda x: not x == -1, [r, g, b]))
+    return sum
+
+
+def main():
+    lines = parse("day2/data.txt")
+    sum_id = id_sum(lines)
+    smallest = smallest_power(lines)
+    print(f"Sum of IDs from valid games: {sum_id}")
+    print(f"Sum of the smallest possible powers: {smallest}")
 
 
 main()
