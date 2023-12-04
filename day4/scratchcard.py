dc51cd702654f78025c2d896ada6b613d43769cf
Eli Roslöf <eli.roslof@gmail.com>
2023-12-04 08:44:01 +0200

    Add second part of day 4.

--- day4/scratchcard.py
+++ day4/scratchcard.py
@@ -15,17 +15,44 @@ def parse(filename):
     return list(lines)
 
 
-def main():
-    lines = parse("day4/data.txt")
+# find all the winning numbers in a card
+def find_matches(line):
+    split_line = line.split(" | ")
+    winning = split_line[0].split()[2:]
+    card_numbers = split_line[1].split()
+    found_winning = list(filter(lambda n: n in card_numbers, winning))
+
+    return found_winning
+
+
+def card_values(lines):
     point_total = 0
     # go through each line, find the winning numbers present in the card, and calculate the point total
     for line in lines:
-        split_line = line.split(" | ")
-        winning = split_line[0].split()[2:]
-        card_numbers = split_line[1].split()
-        found_winning = list(filter(lambda n: n in card_numbers, winning))
+        found_winning = find_matches(line)
         point_total += reduce(lambda x, y: max(x * 2, 1), found_winning, 0)
+    return point_total
+
+
+def card_copies(lines):
+    # keep track of the number of copies of each card
+    copies = [1] * len(lines)
+    for line in lines:
+        card_id = int(line.split(":")[0].split()[1])
+        card_count = copies[card_id - 1]
+        win_count = len(find_matches(line))
+        for i in range(0, card_count):
+            for j in range(card_id, card_id + win_count):
+                copies[j] += 1
+    return reduce(lambda x, y: x + y, copies)
+
+
+def main():
+    lines = parse("day4/data.txt")
+    point_total = card_values(lines)
+    copy_count = card_copies(lines)
     print(f"Value of the scratchcards: {point_total}")
+    print(f"Total number of card copies: {copy_count}")
 
 
 main()
