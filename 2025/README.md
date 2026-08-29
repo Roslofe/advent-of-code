8fc745af95fcab4f64600a4687ff2324f6285142
Eli Roslöf <eli.roslof@gmail.com>
2026-08-29 15:28:12 +0300

    Add part 2 of day 5

--- 2025/README.md
+++ 2025/README.md
@@ -8,4 +8,4 @@
 | 2   | :white_check_mark: | :white_check_mark: |
 | 3   | :white_check_mark: | :white_check_mark: |
 | 4   | :white_check_mark: | :white_check_mark: |
-| 5   | :white_check_mark: |                    |
+| 5   | :white_check_mark: | :white_check_mark: |
--- 2025/day05/ranges.py
+++ 2025/day05/ranges.py
@@ -31,5 +31,47 @@ def part1():
 
   print(f"Day 5 part 1:\n\tNumber of fresh ingredients: {fresh_ingredients}\n\tDuration: {end_time - start_time:.3f}s")
 
+def part2():
+  start_time = time.time()
+  
+  with open("./2025/day05/data.txt", "r") as file:
+    lines = file.readlines()
+
+  ranges = []
+  for i in range(0, len(lines)):
+    line = lines[i]
+    if line == "\n":
+      break
+
+    start, end = line.strip().split("-")
+    ranges.append((int(start), int(end)))
+
+  ranges.sort(key=lambda n: n[0])
+  full_ranges = []
+
+  for s, e in ranges:
+    processed = False
+    for i, (other_s, other_e) in enumerate(full_ranges):
+      # other s is always more than s
+      if s <= other_e and e <= other_e:
+        processed = True
+        break
+      elif s <= other_e:
+        full_ranges[i] = (other_s, e)
+        processed = True
+        break
+
+    if not processed:
+      full_ranges.append((s, e))
+
+  fresh_range_count = 0
+  for s, e in full_ranges:
+    fresh_range_count += (e - s) +1
+
+  end_time = time.time()
+
+  print(f"Day 5 part 2:\n\tNumber of fresh ingredients IDs: {fresh_range_count}\n\tDuration: {end_time - start_time:.3f}s")
+
 if __name__ == "__main__":
   part1()
+  part2()
